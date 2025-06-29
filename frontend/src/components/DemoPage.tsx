9c3b33424fb91484a13a1a9c4a69b7d472f1b8ca
jnjerin <njerijoan24@gmail.com>
2025-06-29 17:01:28 +0300

    Create individual chat message bubble component

--- frontend/src/components/DemoPage.tsx
+++ frontend/src/components/DemoPage.tsx
@@ -6,7 +6,7 @@
 
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { DemoService } from '../types';
+import type { DemoService } from '../types/types';
 import { ChatWidget } from './ChatWidget';
 import { logger, trackEvent } from '../utils/logger';
 
